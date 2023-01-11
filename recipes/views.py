6ca3125266f9d8a889b5e3c21e8564ca621bb02f
AndreMayerT <andremayerusa@gmail.com>
2023-01-11 11:49:42 -0300

    add templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,8 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(req):
-    return HttpResponse('Home')
-
-
-def contact(req):
-    return HttpResponse('Contact')
-
-
-def about(req):
-    return HttpResponse('About')
+    return render(req, 'recipes/home.html')
