f8ee088a73ff4a6189b83ef3b80a70905939d866
AndreMayerT <andremayerusa@gmail.com>
2023-01-19 16:21:41 -0300

    fix null recipes and authors

--- recipes/views.py
+++ recipes/views.py
@@ -7,8 +7,16 @@ from utils.recipes.factory import make_recipe
 
 
 def home(req):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(req, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(req, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+    return render(req, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
